ae7fd4770ebc89cb9a9f1102d86a825af54d1c27
KondratenkoS <sergeikhell@gmail.com>
2023-11-06 15:01:04 +0200

    event output by ID

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
     #[Route('/event/{id}', name: 'event', requirements: ['id' => '[1-9+]*'])]
-    public function index(int $id): Response
+    public function index(int $id, EventRepository $eventRepository): Response
     {
-        return $this->render('event/event-page.html.twig');
+        $event = $eventRepository->find($id);
+        dump($event);
+        return $this->render('event/event-page.html.twig', ['event' => $event]);
     }
 }
